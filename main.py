5c0aa76ad91a6b36c964cc101a8cfc659295a14d
Qobiljon <madrahimovq@gmail.com>
2025-08-05 17:05:27 +0500

    Car nomli class yozdik

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 users = ["Toxir", "Sobir"]
 print(users[0])
 
+# --------------
+
 
 def say_hello(name: str) -> str:
     return f"Assalomu alaykum {name}"
 
 
 print(say_hello("Toxir"))
+
+
+# -------------
+
+
+class Car:
+    def __init__(self, model: str, brand: str, price: int, color: str):
+        self.model = model
+        self.brand = brand
+        self.price = price
+        self.color = color
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
